f620964239336a01422a3244cf4c0cde48b9f698
out1Man <88752313+out1Man@users.noreply.github.com>
2021-09-19 08:14:36 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,54 @@
+﻿#include "game.h"
+void menu()
+{
+	printf("******************\n");
+	printf("**** 1. play *****\n");
+	printf("**** 0. exit *****\n");
+	printf("******************\n");
+}
+void game()
+{
+	char board[ROWS][COLS] = { 0 };
+	char showboard[ROWS][COLS] = { 0 };
+	//初始化
+	initboard(board, ROWS, COLS, '0');
+	initboard(showboard, ROWS, COLS, '*');
+	//打印
+	//printboard(board, ROW, COL);
+	printboard(showboard, ROW, COL);
+	//设置雷
+	setboard(board, ROW, COL);
+	//printboard(board, ROW, COL);
+	//扫雷
+	findboard(board, showboard, ROW, COL);
+}
+
+void test()
+{
+	srand((unsigned int)time(NULL));
+	while (1)
+	{
+		menu();
+		int a = 0;
+		printf("请选择");
+		scanf("%d", &a);
+		if (a == 1)
+		{
+			game();
+		}
+		else if (a == 0)
+		{
+			printf("再见");
+			break;
+		}
+		else
+		{
+			printf("请重新选择");
+		}
+	}
+}
+int main()
+{
+	test();
+	return 0;
+}
